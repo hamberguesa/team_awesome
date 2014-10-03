8ce22213f4a500a7726d3d2115057ab1b1eaf1f0
Samantha Drago <sfdrago@Samanthas-MacBook-Air-2.local>
2014-10-03 15:49:04 -0700

    finished

--- source/team_awesome/app/controllers/controller.rb
+++ source/team_awesome/app/controllers/controller.rb
@@ -1,238 +1,199 @@
 require_relative '../models/ingredient'
 require 'yummly'
 require 'launchy'
+require 'date'
+require 'colorize'
 
 module Model
   def self.all_ingredients
     Ingredient.all
   end
 
-  def self.format_search_terms(array)
-    query = []
-    array.each do |id|
-      query <<Ingredient.find(id.to_i).name
+  def self.format_search_terms(ingredients, diet)
+    ingredients = ingredients.join(", ")
+
+    case diet[0].downcase
+    when 'vegetarian'
+      diet_string = '387^Lacto-ovo vegetarian'
+    when 'vegan'
+      diet_string = '386^Vegan'
+    when 'pescetarian'
+      diet_string = '390^Pescetarian'
+    else
+      diet_string = ''
     end
-    query.join(", ")
+
+    [ingredients, {'allowedDiet' => diet_string}]
   end
 
-  def self.query(string)
-    results = Yummly.search(string)
+  def self.query(array)
+    results = Yummly.search(array[0], array[1])
     parsed = results.matches[0..4]
   end
 
   def self.get_recipe_names(parsed_recipes)
     @@recipes = {}
     parsed_recipes.each_with_index do |recipe, index|
-      @@recipes.merge!(index => [recipe["id"], recipe["recipeName"]] )
+      @@recipes.merge!(index => [recipe["id"], recipe["recipeName"], recipe["rating"]] )
     end
     @@recipes
   end
 
   def self.fetch_html(number)
-    recipe_id = @@recipes[number.to_i][0]
+    recipe_id = @@recipes[number][0]
     url = "http://www.yummly.com/recipe/#{recipe_id}"
-    result
+    Launchy.open(url)
+  end
+
+  def self.add_ingredient(ingredient_array)
+    ingredient_array.each do |ingredient|
+        Ingredient.create(name: ingredient, expiration_date: (Date.today + 7))
+    end
+  end
+
+  def self.delete_ingredient(name_array)
+    name_array.each do |name|
+        Ingredient.find_by_name(name).destroy
+    end
+  end
 end
 
 module Controller
   def self.run
+    View.logo
+    View.spacer
     View.display_pantry(Model.all_ingredients)
-    answer = View.options.downcase.split(" ")
-    command = answer.shift
+    View.spacer
+    command = View.options.downcase
     case command
     when 'cook'
-      parsed = Model.query(Model.format_search_terms(answer))
+      answer = View.get_input(command)
+      View.spacer
+      diet = View.get_diet
+      View.spacer
+      View.hacking_the_mainframe
+      parsed = Model.query(Model.format_search_terms(answer, diet))
+      View.spacer
       View.display_result_recipes(Model.get_recipe_names(parsed))
+      View.spacer
       Model.fetch_html(View.pick_recipe)
+    when 'add'
+      answer = View.get_input(command)
+      View.spacer
+      Model.add_ingredient(answer)
+      View.add_confirmation(answer)
+      View.display_pantry(Model.all_ingredients)
+    when 'delete'
+      answer = View.get_input(command)
+      View.spacer
+      Model.delete_ingredient(answer)
+      View.delete_confirmation
+      View.display_pantry(Model.all_ingredients)
     end
   end
 end
 
 module View
   def self.display_pantry(results)
-    puts 'You currently have the following ingredients in your pantry:'
+    puts 'You currently have the following ingredients in your pantry:'.center(159)
     results.each do |object|
-      puts "#{object.id}. #{object.name} - Expires #{object.expiration_date}"
+      puts "#{object.name.capitalize} - (exp. #{object.expiration_date})".center(159)
     end
   end
 
   def self.options
-    puts 'Please choose from one of the following options:'
-    puts '1. To add an ingredient, type \'add\'.'
-    puts '2. To delete an ingredient, type \'delete\' followed by the number of the ingredient you would like to remove.'
-    puts '3. To see possible recipes, type \'cook\' followed by 3 ingredients you would like to use.'
-    gets.chomp
+    puts 'Please choose from one of the following options:'.center(159)
+    puts '* To add an ingredient, type \'add\' *'.center(159)
+    puts '* To delete an ingredient, type \'delete\' *'.center(159)
+    puts '* To see possible recipes, type \'cook\' *'.center(159)
+    puts
+    print" "*70
+    answer = gets.chomp
+    puts
+    answer
   end
 
   def self.display_result_recipes(recipe_hash)
-    puts 'Here is what you can cook!'
+    puts
+    puts
+    puts "Here is what you can cook!".center(159)
+    puts
     recipe_hash.each do |key, value|
-      puts "#{key}. #{value[1]}"
+      puts "#{key + 1}. #{value[1]} - Rating: #{value[2]}/5 stars".center(159)
     end
+    puts
+  end
+
+  def self.hacking_the_mainframe
+    puts
+    puts
+    puts
+    puts "HACKING THE MAINFRAME c/o Yummly.com".colorize(:green).center(159)
+    puts
+    159.times  { print '.'.colorize(:green); sleep 0.01}
+    puts
+    puts
+  end
+
+  def self.spacer
+    spacer = '-'*50
+    print spacer.colorize(:light_blue).center(173)
   end
 
   def self.pick_recipe
-    puts "Which recipe would you like to cook? Enter its number."
-    choice = gets.chomp
+    puts "Which recipe would you like to cook? Enter its number.".center(159)
+    puts
+    print" "*70
+    answer = gets.chomp.to_i - 1
+    puts
+    answer
+  end
+
+  def self.add_confirmation(ingredients)
+    puts "You just added #{ingredients.join(", ")} to your pantry! Time to get cookin'!".center(159)
+  end
+
+  def self.delete_confirmation
+    puts "You just removed some food from your pantry. Time to go shoppin'!".center(159)
+  end
+
+  def self.get_input(command)
+    puts "What would you like to #{command}?".center(159)
+    puts
+    print" "*70
+    answer = gets.chomp.split(", ")
+    puts
+    answer
+  end
+
+  def self.get_diet
+    puts "Which word best describes your diet?".center(159)
+    puts
+    puts 'Vegetarian     Vegan     Pescetarian     Anything!'.center(159)
+    puts
+    print" "*70
+    answer = gets.chomp.split(", ")
+    puts
+    answer
+  end
+
+  def self.logo
+    logo =  <<-LOGO
+   .----------------. .----------------. .----------------. .----------------. .----------------. .----------------. .----------------. .----------------.
+  | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. |
+  | |  _________   | | |  _______     | | |     _____    | | |  ________    | | |    ______    | | |  _________   | | |   _____      | | |  ____  ____  | |
+  | | |_   ___  |  | | | |_   __ \\    | | |    |_   _|   | | | |_   ___ `.  | | |  .' ___  |   | | | |_   ___  |  | | |  |_   _|     | | | |_  _||_  _| | |
+  | |   | |_  \\_|  | | |   | |__) |   | | |      | |     | | |   | |   `. \\ | | | / .'   \\_|   | | |   | |_  \\_|  | | |    | |       | | |   \\ \\  / /   | |
+  | |   |  _|      | | |   |  __ /    | | |      | |     | | |   | |    | | | | | | |    ____  | | |   |  _|  _   | | |    | |   _   | | |    \\ \\/ /    | |
+  | |  _| |_       | | |  _| |  \\ \\_  | | |     _| |_    | | |  _| |___.' / | | | \\ `.___]  _| | | |  _| |___/ |  | | |   _| |__/ |  | | |    _|  |_    | |
+  | | |_____|      | | | |____| |___| | | |    |_____|   | | | |________.'  | | |  `._____.'   | | | |_________|  | | |  |________|  | | |   |______|   | |
+  | |              | | |              | | |              | | |              | | |              | | |              | | |              | | |              | |
+  | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' |
+   '----------------' '----------------' '----------------' '----------------' '----------------' '----------------' '----------------' '----------------'
+ LOGO
+ puts logo.colorize(:blue)
+ puts
+ puts
   end
 
 end
-
-# module Controller
-
-#   def self.run
-#     View.menu
-#     command = gets.chomp.downcase
-#     if command == "list all"
-#       answer = View.which_list.downcase
-#       if answer == "all"
-#         View.print(Task.all)
-#       else
-#         View.print(Task.where(list_id: answer))
-#       end
-#     elsif command == "list outstanding"
-#       tasks = Task.all.sort { |task1, task2| task1.due_date <=> task2.due_date }
-#       tasks.reject! { |task| task.completed == true}
-#       View.print(tasks)
-#     elsif command == "list completed"
-#       tasks = Task.all.sort { |task1, task2| task1.due_date <=> task2.due_date }
-#       tasks.reject! { |task| task.completed == false}
-#       View.print(tasks)
-#     elsif command == "list by tag"
-#       tag = View.which_tag.to_i
-#       tasks = []
-#       Task.all.each do |task|
-#         if task.tag_ids.include?(tag)
-#           tasks << task
-#         end
-#       end
-#       View.print(tasks)
-#     elsif command == "add list"
-#       list = List.create(name: View.what)
-#       View.new_list_confirmation(list.name)
-#     elsif command == "add task"
-#       list = View.which_list.downcase
-#       task = Task.create(name: View.what, list_id: list)
-#       View.new_task_confirmation(task.name)
-#     elsif command == "add tag"
-#       id = View.which_task
-#       tag = View.which_tag
-#       task = Task.find(id).tags
-#       task << Tag.find(tag)
-#       View.tag_confirmation(id, tag)
-#     elsif command == "delete list"
-#       list = View.which_list
-#       List.find(list).tasks.each do |task|
-#         task.destroy
-#       end
-#       List.find(list).destroy
-#       View.deleted_confirmation
-#     elsif command == "delete task"
-#       Task.find(View.which_task).destroy
-#       View.deleted_confirmation
-#     elsif command == "complete"
-#       task = Task.find(View.which_task)
-#       task.completed = true
-#       task.save
-#       View.task_completed_confirmation
-#     else
-#       View.error
-#     end
-#   end
-# end
-
-# module View
-
-#   def self.menu
-#     puts "Welcome to The To Do App. Here's what you can do:"
-#     puts "-------------------------------------------------"
-#     puts "list all: View all tasks from one or all of your lists"
-#     puts "list outstanding: View outstanding tasks sorted by due date"
-#     puts "list completed: View completed tasks sorted by completed date"
-#     puts "list by tag: View all tasks associated with a tag"
-#     puts "-------------------------------------------------"
-#     puts "add list: Adds a new todo list"
-#     puts "add task: Adds a task to one of your lists"
-#     puts "add tag: Adds a tag to one of your tasks"
-#     puts "-------------------------------------------------"
-#     puts "delete list: Deletes a list"
-#     puts "delete task: Deletes a task from one of your lists"
-#     puts "-------------------------------------------------"
-#     puts "complete: Marks a task as completed"
-#     puts "-------------------------------------------------"
-#   end
-
-#   def self.print(tasks)
-#     count = 1
-#     puts "-------------------------------------------------"
-#     tasks.each do |task|
-#       puts "#{count}. #{task.name} (ID: #{task.id}) - Due Date: #{task.due_date} | Completed: #{task.completed}"
-#       count += 1
-#     end
-#   end
-
-#   def self.which_list
-#     puts "Which list?"
-#     puts "-------------------------------------------------"
-#     List.all.each do |list|
-#       puts "#{list.name} (ID: #{list.id})"
-#     end
-#       puts "all"
-#     gets.chomp
-#   end
-
-#   def self.which_task
-#     puts "Which task?"
-#     puts "-------------------------------------------------"
-#     Task.all.each do |task|
-#       puts "#{task.name} (ID: #{task.id})"
-#     end
-#     gets.chomp
-#   end
-
-#   def self.which_tag
-#     puts "Which tag?"
-#     puts "-------------------------------------------------"
-#     Tag.all.each do |tag|
-#       puts "#{tag.name} (ID: #{tag.id})"
-#     end
-#     gets.chomp
-#   end
-
-#   def self.what
-#     puts "What would you like to add?"
-#     puts "-------------------------------------------------"
-#     gets.chomp
-#   end
-
-#   def self.new_task_confirmation(name)
-#     puts "You just added '#{name}' to your to do list!"
-#     puts "-------------------------------------------------"
-#   end
-
-#   def self.new_list_confirmation(name)
-#     puts "You just added '#{name}' as a new list!"
-#     puts "-------------------------------------------------"
-#   end
-
-#   def self.deleted_confirmation
-#     puts "Deleted!"
-#     puts "-------------------------------------------------"
-#   end
-
-#    def self.tag_confirmation(task, tag)
-#     puts "Tagged #{Task.find(task).name} with #{Tag.find(tag).name}"
-#     puts "-------------------------------------------------"
-#   end
-
-#   def self.task_completed_confirmation
-#     puts "Way to go! Great job on completing a task!"
-#     puts "-------------------------------------------------"
-#   end
-
-#   def self.error
-#     puts "I don't know that command. Try 'list', 'add task', 'add list', 'delete task', 'delete list' or 'complete'."
-#     puts "-------------------------------------------------"
-#   end
-
-# end
